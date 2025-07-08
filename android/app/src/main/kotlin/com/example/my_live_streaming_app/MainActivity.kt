1af14c929c54dbf4d2d4a800a2e3308abfe71885
Parinya <lovenicenoona22@email.com>
2025-07-08 22:07:13 +0700

    ✅ Migrate to Android v2 embedding (FlutterActivity)

--- android/app/src/main/kotlin/com/example/my_live_streaming_app/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/my_live_streaming_app/MainActivity.kt
@@ -1,5 +1,6 @@
-package com.example.my_live_streaming_app
+package com.example.my_live_streaming_app  // เปลี่ยนตาม namespace จริง
 
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity : FlutterActivity()
+class MainActivity: FlutterActivity() {
+}
